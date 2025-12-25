7d2de8443a656532e7ff7593ef9655f62e935bad
bhawna202004 <bgarg6675@gmail.com>
2025-12-25 14:42:45 +0000

    conf done

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,15 @@
+const accountId = 123444
+let accountEmail = "abc154@googlr.com"
+var accountPassword = "1234"
+accountCity = "jaipur"
+let accountState
+
+// accountId = 23
+accountEmail = "gggg231@google.com"
+console.log(accountId);
+console.log(accountEmail);
+/*
+prefer not to use var
+bcz of issue of functional scope and block scope
+*/
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all js code as newer js version
+
+// alert("hello") // we are using nodejs not browser
+
+let name = "bhawna"
+let age = 21
+let isLoggedIn = false
+let state = null
+
+// nummer => 2 to power 52
+// bigInt 
+// string
+// boolean => true/false
+// null => standalone value 
+// undefined => 
+// symbol => unique 
+//object 
+
+console.log(typeof name);
+console.log(typeof null); //object
+console.log(typeof undefined);  // undefined
+
