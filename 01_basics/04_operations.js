aaea509a9fa3e9713d170fdb43d47d421bf9a40c
bhawna202004 <bgarg6675@gmail.com>
2025-12-30 13:37:37 +0000

    done

---
+++ 01_basics/04_operations.js
@@ -0,0 +1,19 @@
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2**3);  // ** used for power ; 2**3 = 8
+// console.log(2%3);  // ** used for reminder ; 2%3 = 2
+
+let str1 = "hello"
+let str2 = " world"
+
+let str3 = str1 + str2
+
+console.log(str3);
+
+console.log("1"+ 2);  // 12
+console.log(1 + "2");   // 12 
+console.log("1"+ "2");    // 12
+console.log("1"+ 2 + 2);  // 122
+console.log(1 + 2 + "3");  // 33
