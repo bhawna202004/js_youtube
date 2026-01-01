c3aa60c10900fc51153d106d326b4a723215624b
bhawna202004 <bgarg6675@gmail.com>
2026-01-01 17:09:46 +0000

    conf done

--- 01_basics/datatypeSummary.js
+++ 01_basics/datatypeSummary.js
@@ -1,9 +1,76 @@
+
 /* # PRIMITIVE DATATYPE- 7 type
 String, Number, Boolean, null, undefined, Symbol, bigInt
 */
+
+let str = "bhawna"  // string
+const score = 100  // number
+const scoreValue = 100.3 // number
+const isLoggedIn = false  // boolean
+const outsideTemp = null    // null
+let value;     // undefined
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId); // confirms that both values are taken different 
+
+const bigNumber = 23456666666644322654n
+
 
 
 /* REFERENCE DATATYPE  (non-primitive) -
 Arrays, Objects, functions
 */
 
+const heros = ["batman", "spiderman", "women"]  //array
+
+//objects
+let myObj = {
+ name : "bhawna",
+age : 21,
+};
+
+// function
+function myFunction() {
+    console.log("hello world ");
+}
+
+
+// to check type of datatype
+console.log(typeof id);
+
+
+/*
+
+type of:                                 "value":
+# string                                 "string"
+# number                                 "number"
+# undefined                             "undefined"
+# null                                   "object"
+# boolean                                "boolean"
+# object(native or                       "object"
+  does not implement)
+# object(native or                      "function"
+  host and does implement)
+
+  */
+
+
+  //    *********** memory **************
+
+  // stack  (primitive) - it return copy
+  let email = "bgarg2344@yaahooo.com"
+  let identity = email
+  identity = "aadhar card"
+
+  console.log(email);
+  console.log(identity);
+  
+  //heap   (non-primitive)  it return reference(original)
+
+  let user1 = {
+    user = "user@google.com",
+    upiId = "user@ybl",
+  };
+  
