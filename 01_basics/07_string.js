30c39817e5d51caaf54a1233835b8df2370d58e6
bhawna202004 <bgarg6675@gmail.com>
2026-01-04 12:48:32 +0000

    done

--- 01_basics/07_string.js
+++ 01_basics/07_string.js
@@ -8,10 +8,29 @@ console.log("my name is " + name + "and my age is " + age );  // old and boring 
 console.log(`my name is ${name} and my age is ${age}.`);  // modern method
  
 
-let gameName = new String("BHAWNA GARG")
+let gameName = new String("BHAWNA-GARG")
 
 console.log(gameName[4]);
 console.log(gameName.__proto__);
 console.log(gameName.length);
 console.log(gameName.toLowerCase());
-console.log(gameName.split());
+console.log(gameName.charAt(5));
+console.log(gameName.indexOf('A'));
+
+const newString = gameName.substring(0,5)
+console.log(newString);
+
+const anotherstring = gameName.slice(-9,5) 
+console.log(anotherstring);
+
+console.log(gameName.split('-'));  // split the sentences on basis of specified character
+console.log(gameName.bold());
+
+const str1 = "          hello          "
+console.log(str1);
+console.log(str1.trim());   // trim remove the extra space - trimEnd and trimStart
+
+let url = "http://bhawna.com/bhawna%20garg"
+console.log(url.replace('%20','-'));    // replace 
+console.log(url.includes('sneha'));     // tells specified element is present or not
+
---
+++ 01_basics/08_nums-and-maths.js
@@ -0,0 +1,34 @@
+
+//+++++++++++++++++++++++ NUMBERS +++++++++++++++++++++
+
+const score =   400.333
+// console.log(score);
+
+const balance = new Number(300)
+// console.log(balance);
+// console.log(balance.toFixed(2));
+// console.log(balance.toExponential());
+// console.log(balance.toString());  // it convert num into string and now it use all methods of string
+
+// console.log(score.toPrecision(4));
+// console.log(score.toPrecision(2));  // convert it automatically in exponential
+
+const hundreds = 100000000
+// console.log(hundreds.toLocaleString());    // BY deafult read in US standard
+// console.log(hundreds.toLocaleString('en-IN'));  // read in indian standard
+// console.log(Number.EPSILON);
+
+
+
+// ++++++++++++++++++++++ MATHS ++++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-40));       // 40
+// console.log(Math.round(40.855));  //41 ( give round value )
+// console.log(Math.ceil(40.35));    // 41 ( give round upper value )
+// console.log(Math.floor(40.34));   // 40  (give round lower value)
+// console.log(Math.log(2));
+// console.log(Math.sqrt(81));   // 9 
+
+
+console.log(Math.random());
