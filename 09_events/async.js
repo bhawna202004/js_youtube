0a8b3634e7f1e413cd6c775a59b516e4fef1c813
gaurav-garg007 <ggarg6406@gmail.com>
2026-02-14 23:21:52 +0530

    ok

---
+++ 09_events/async.js
@@ -0,0 +1,7 @@
+/*
+blocking code and non blocking code
+blocking code - block the flow of program - read file sync
+non blocking code - doesn't block the execution - read file async
+
+
+*/
--- 09_events/event_basics.js
+++ 09_events/event_basics.js
@@ -1,22 +1,66 @@
 // js is sequential running
 
 // document.getElementById('owl').onclick = function(){
-//     alert("owl clicked")
+//     alert("owl clicked")               // by click funciton works
 // }
 
-document.getElementById('owl').addEventListener('click',function () {
-    alert('owl clicked again')
-})
 
-// attachevent() 
-// jQuery - .on
+
+// document.getElementById('owl').addEventListener('click',function () {
+//     alert('owl clicked again')
+// })
+
+
+
+// attachEvent()  --->  used to run internal explorer
+// on() --->  used in jquery, instead of addEventListener()  
 
 document.getElementById('river').addEventListener('click',function (e) {
-     console.log(e);
-     
+     console.log(e);                        // give all events in browser console
 }, false) 
 
 // type, timestamp, defaultPrevented, target, toElement
 // srcElement, currentTarget, clientX, screenX, screenY
 // altKey, ctrlKey, shiftkey, keycode
 
+
+// event propogation ---> 
+//  event bubbling (false used)-- bottom to top  --->  small to large
+// and event capturing (true used)-- top to bottom   ---> large to small
+
+
+// document.getElementById('images').addEventListener('click',function(e){
+//     console.log("clicked inside the ul"); 
+// }, false)
+
+
+
+// document.getElementById('owl').addEventListener('click',function(e){
+//     console.log("owl clicked"); 
+//     e.stopPropagation()      // stop bubbling - not go to top element
+// }, false)
+
+
+
+// document.getElementById('google').addEventListener('click', function(e){
+//     e.preventDefault(); 
+//     e.stopPropagation();
+//     console.log("goggled clicked");
+    
+// }, false)
+
+
+document.querySelector('#images').addEventListener('click', function(e){
+   console.log(e.target);       // show target value
+   console.log(e.target.parentNode);    // return target with its parent
+   console.log(e.target.tagName);      // return target tagname
+   if(e.target.tagName === 'IMG'){
+    console.log(e.target.id);
+    
+    let removeIt = e.target.parentNode
+    removeIt.remove()
+    }
+  
+  // removeIt.parentNode.removeChild(removeIt)
+
+}, false)
